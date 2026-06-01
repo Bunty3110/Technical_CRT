aa174fd2f1efad3b7636860c9dbfff99aa3a9ac3
BUNTY3110 <1ampraveenpardhi@gmail.com>
2026-06-01 13:35:04 +0530

    day5

---
+++ DSA_search/linear_search.py
@@ -0,0 +1,10 @@
+def linear_search(arr, target, size):
+    for i in range(size):
+        if arr[i] == target:
+            return i
+    return -1
+arr=[1, 2, 3, 4, 5,6,7,8,9,10]
+size=len(arr)
+target= 7
+index=linear_search(arr,target,size)
+print(f"Element {target} is at index: {index}")
